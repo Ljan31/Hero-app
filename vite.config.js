b828c9be4b446ee8c6e8f86519f8400746ff74b1
Ljan31 <mercen2021@gmail.com>
2024-08-30 12:44:08 -0400

    updated02

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:['https://ljan31.github.io/Hero-app/']
+  base:['https://ljan31.github.io/Hero-app']
 })
